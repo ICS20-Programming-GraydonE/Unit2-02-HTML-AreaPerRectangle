5a4623b83872a9cf04b0aa933487b840277a4c57
GraydonE1 <124379361+GraydonE@users.noreply.github.com>
2023-03-10 02:31:04 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// Created by: Graydon
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('area').innerHTML = 'The area is ' + (4 * 5)
+}
+
+function doMathClicked1 () {
+  // this function does basic math
+  
+  document.getElementById('per').innerHTML = 'The perimiter is ' + (5*2+2*4)
+}
